3b7e98cdd82ac2c0b960bf0dd7c2d62b4ef82f49
Dana Merrick <dana.merrick@gmail.com>
2021-01-17 19:53:14 -0500

    Progress

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,6 +1,7 @@
 # traefik
 helm repo add traefik https://helm.traefik.io/traefik
 helm upgrade --install traefik traefik/traefik -f k8s/traefik/stage-1/config.yml -n kube-system
+kubectl apply -k k8s/traefik/stage-1
 
 # external-dns
 helm repo add bitnami https://charts.bitnami.com/bitnami
@@ -9,10 +10,10 @@ kubectl apply -k k8s/external-dns/stage-1
 
 # cert-manager
 helm repo add jetstack https://charts.jetstack.io
-helm upgrade --install cert-manager --namespace kube-system jetstack/cert-manager -f k8s/cert-manager/stage-1/config.yml
+helm upgrade --install cert-manager jetstack/cert-manager -f k8s/cert-manager/stage-1/config.yml -n kube-system
 kubectl apply -k k8s/cert-manager/stage-1
 
 # k8s-dashboard
 helm repo add kubernetes-dashboard https://kubernetes.github.io/dashboard/
-helm upgrade --install k8s-dashboard kubernetes-dashboard/kubernetes-dashboard -n kube-system -f k8s/k8s-dashboard/stage-1/config.yml
+helm upgrade --install k8s-dashboard kubernetes-dashboard/kubernetes-dashboard -f k8s/k8s-dashboard/stage-1/config.yml -n kube-system
 kubectl apply -k k8s/k8s-dashboard/stage-1
