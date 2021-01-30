aef50b447e06fd73bd3e3ebec1ebaee3337fcf56
Dana Merrick <dana.merrick@gmail.com>
2021-01-30 15:30:13 -0500

    Adding CI user

---
+++ terraform/stage-1/ci.tf
@@ -0,0 +1,72 @@
+resource aws_iam_user ci {
+  name = "CIUser"
+  path = "/bots/"
+  tags = {
+    Name = "CIUser"
+  }
+  force_destroy = false
+}
+
+data aws_iam_policy_document ci_service_account_policy {
+
+  statement {
+    actions = ["sts:AssumeRole"]
+    effect  = "Allow"
+
+    principals {
+      identifiers = [
+        aws_iam_user.ci.arn #, "arn:aws:iam::${data.aws_caller_identity.current.account_id}:role/ReverieContinuousIntegrationRole"
+      ]
+      type = "AWS"
+    }
+  }
+}
+
+resource aws_iam_role ci {
+  name               = "CIRole"
+  assume_role_policy = data.aws_iam_policy_document.ci_service_account_policy.json
+}
+
+
+resource aws_iam_policy ci {
+  name   = "AllowAccessForContinuousIntegration"
+  policy = <<EOF
+{
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+            "Sid": "S3ReadOnlyAccess",
+            "Action": [
+              "s3:GetObject",
+              "s3:HeadBucket",
+              "s3:ListBucket"
+            ],
+            "Effect": "Allow",
+            "Resource": [
+                "${aws_s3_bucket.static_website.arn}",
+                "${aws_s3_bucket.static_website.arn}/*"
+            ]
+        }
+    ]
+}
+EOF
+}
+
+# give the CI user access to the policy
+resource aws_iam_user_policy_attachment ci {
+  policy_arn = aws_iam_policy.ci.arn
+  user       = aws_iam_user.ci.name
+}
+
+# give the CI role access to the policy
+resource aws_iam_role_policy_attachment ci_role_access {
+  role       = aws_iam_role.ci.name
+  policy_arn = aws_iam_policy.ci.arn
+}
+
+# create an access key so we can use it in k8s
+resource aws_iam_access_key ci {
+  user = aws_iam_user.ci.name
+  # encrypt it using the @adanalife keybase key
+  pgp_key = "keybase:adanalife"
+}
