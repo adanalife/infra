513f6b91a7c2d496820e6e3d8ffb3d6a5f804241
Dana Merrick <dana.merrick@gmail.com>
2021-01-30 13:39:47 -0500

    Cleanup

--- terraform/core/s3.tf
+++ terraform/core/s3.tf
@@ -30,17 +30,10 @@ resource aws_s3_bucket_public_access_block dashcam_videos {
   restrict_public_buckets = true
 }
 
+# an empty S3 bucket that serves as a redirect
 resource aws_s3_bucket primary_redirect {
   bucket = "${var.domain}-test"
   acl    = "private"
-
-  # server_side_encryption_configuration {
-  #   rule {
-  #     apply_server_side_encryption_by_default {
-  #       sse_algorithm = "AES256"
-  #     }
-  #   }
-  # }
 
   website {
     error_document = "error.html"
@@ -62,28 +55,10 @@ EOF
   }
 }
 
-# prevent this bucket from ever going public
-# resource aws_s3_bucket_public_access_block dashcam_videos {
-#   bucket = aws_s3_bucket.dashcam_videos.id
-
-#   block_public_acls       = true
-#   block_public_policy     = true
-#   ignore_public_acls      = true
-#   restrict_public_buckets = true
-# }
-#
-
+# an empty S3 bucket that serves as a redirect
 resource aws_s3_bucket secondary_redirect {
   bucket = "${var.secondary_domain}-test"
   acl    = "private"
-
-  # server_side_encryption_configuration {
-  #   rule {
-  #     apply_server_side_encryption_by_default {
-  #       sse_algorithm = "AES256"
-  #     }
-  #   }
-  # }
 
   website {
     error_document = "error.html"
@@ -105,13 +80,3 @@ EOF
     Name = "${var.secondary_domain}-test"
   }
 }
-
-# prevent this bucket from ever going public
-# resource aws_s3_bucket_public_access_block dashcam_videos {
-#   bucket = aws_s3_bucket.dashcam_videos.id
-
-#   block_public_acls       = true
-#   block_public_policy     = true
-#   ignore_public_acls      = true
-#   restrict_public_buckets = true
-# }
