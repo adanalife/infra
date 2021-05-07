1d8fee0b175eb12ab9e47f9b7f1d740bd43d8115
Dana Merrick <dana.merrick@gmail.com>
2021-05-07 12:21:20 -0400

    Upgrading

--- terraform/core/versions.tf
+++ terraform/core/versions.tf
@@ -1,12 +1,10 @@
 terraform {
-  required_version = "~> 0.12"
+  required_version = "~> 0.13"
   required_providers {
-    # we don't strictly require v2.65, and should
-    # probably move to v3.0 when it gets released
     # c.p. terraform.io/docs/providers/aws/index.html
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 2.65"
+      version = "~> 3.39"
     }
   }
 
