2749c15b18d04d753f75450754bf6cb008bc8f47
Dana Merrick <dana.merrick@gmail.com>
2021-06-26 14:42:17 -0400

    Progress

--- terraform/stage-1/route53.tf
+++ terraform/stage-1/route53.tf
@@ -25,3 +25,12 @@ resource "aws_route53_record" "primary_static_site" {
     evaluate_target_health = false
   }
 }
+
+resource "aws_route53_record" "tripbot" {
+  zone_id = aws_route53_zone.primary_subdomain_zone.zone_id
+  name    = "tripbot.${aws_route53_zone.primary_subdomain_zone.name}"
+  type    = "CNAME"
+  ttl     = "300"
+  records = [aws_instance.tripbot.public_dns]
+}
+
