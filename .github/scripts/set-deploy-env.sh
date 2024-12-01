ab17574b1fff604660b1561f758c18d082789273
Dana Merrick <dmerrick@ginkgobioworks.com>
2024-12-01 11:40:39 -0600

    ok

--- .github/scripts/set-deploy-env.sh
+++ .github/scripts/set-deploy-env.sh
@@ -1,28 +1,27 @@
-#!/usr/bin/env bash
+#!/bin/bash
 
 # Set DEPLOY_ENV based on the input directory
 case $1 in
   terraform/core)
-    #echo "DEPLOY_ENV=core" >> $GITHUB_STATE
-    echo "AWS_ACCESS_KEY_ID=${{ secrets.CI_AWS_ACCESS_KEY_ID }}" >> $GITHUB_ENV
-    echo "AWS_SECRET_ACCESS_KEY=${{ secrets.CI_AWS_SECRET_ACCESS_KEY }}" >> $GITHUB_ENV
-    #echo "AWS_ACCESS_KEY_ID=${{ secrets.CI_CORE_AWS_ACCESS_KEY_ID }}" >> $GITHUB_ENV
-    #echo "AWS_SECRET_ACCESS_KEY=${{ secrets.CI_CORE_AWS_SECRET_ACCESS_KEY }}" >> $GITHUB_ENV
+    echo "DEPLOY_ENV=core" >> $GITHUB_ENV
+    echo "AWS_ACCESS_KEY_ID=${CI_CORE_AWS_ACCESS_KEY_ID}" >> $GITHUB_ENV
+    echo "AWS_SECRET_ACCESS_KEY=${CI_CORE_AWS_SECRET_ACCESS_KEY}" >> $GITHUB_ENV
     ;;
   terraform/prod-1)
-    #echo "DEPLOY_ENV=prod" >> $GITHUB_STATE
-    echo "AWS_ACCESS_KEY_ID=${{ secrets.CI_PROD_AWS_ACCESS_KEY_ID }}" >> $GITHUB_ENV
-    echo "AWS_SECRET_ACCESS_KEY=${{ secrets.CI_PROD_AWS_SECRET_ACCESS_KEY }}" >> $GITHUB_ENV
+    echo "DEPLOY_ENV=prod" >> $GITHUB_ENV
+    echo "AWS_ACCESS_KEY_ID=${CI_PROD_AWS_ACCESS_KEY_ID}" >> $GITHUB_ENV
+    echo "AWS_SECRET_ACCESS_KEY=${CI_PROD_AWS_SECRET_ACCESS_KEY}" >> $GITHUB_ENV
     ;;
   terraform/stage-1)
-    #echo "DEPLOY_ENV=stage" >> $GITHUB_STATE
-    echo "AWS_ACCESS_KEY_ID=${{ secrets.CI_AWS_ACCESS_KEY_ID }}" >> $GITHUB_ENV
-    echo "AWS_SECRET_ACCESS_KEY=${{ secrets.CI_AWS_SECRET_ACCESS_KEY }}" >> $GITHUB_ENV
-    # echo "AWS_ACCESS_KEY_ID=${{ secrets.CI_STAGE_AWS_ACCESS_KEY_ID }}" >> $GITHUB_ENV
-    # echo "AWS_SECRET_ACCESS_KEY=${{ secrets.CI_STAGE_AWS_SECRET_ACCESS_KEY }}" >> $GITHUB_ENV
+    echo "DEPLOY_ENV=stage" >> $GITHUB_ENV
+    echo "AWS_ACCESS_KEY_ID=${CI_STAGE_AWS_ACCESS_KEY_ID}" >> $GITHUB_ENV
+    echo "AWS_SECRET_ACCESS_KEY=${CI_STAGE_AWS_SECRET_ACCESS_KEY}" >> $GITHUB_ENV
     ;;
   *)
     echo "Unknown environment: $1"
     exit 1
     ;;
 esac
+
+# Save DEPLOY_ENV to GITHUB_STATE
+echo "DEPLOY_ENV=$DEPLOY_ENV" >> $GITHUB_STATE
