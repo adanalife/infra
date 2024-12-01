636bda2d5d8ab3449ee1d25bbb3b55ad0c7ff359
Dana Merrick <dmerrick@ginkgobioworks.com>
2024-11-30 19:46:15 -0600

    add CITerraformRole

--- terraform/stage-1/ci.tf
+++ terraform/stage-1/ci.tf
@@ -7,6 +7,13 @@ resource "aws_iam_user" "ci" {
   force_destroy = false
 }
 
+# create an access key so we can use it in k8s
+resource "aws_iam_access_key" "ci" {
+  user = aws_iam_user.ci.name
+  # encrypt it using the @adanalife keybase key
+  pgp_key = "keybase:adanalife"
+}
+
 data "aws_iam_policy_document" "ci_service_account_policy" {
 
   statement {
@@ -27,7 +34,7 @@ resource "aws_iam_role" "ci" {
   assume_role_policy = data.aws_iam_policy_document.ci_service_account_policy.json
 }
 
-
+#TODO: convert this to the data block format
 resource "aws_iam_policy" "ci" {
   name   = "AllowAccessForContinuousIntegration"
   policy = <<EOF
@@ -73,9 +80,39 @@ resource "aws_iam_role_policy_attachment" "ci_role_access" {
   policy_arn = aws_iam_policy.ci.arn
 }
 
-# create an access key so we can use it in k8s
-resource "aws_iam_access_key" "ci" {
-  user = aws_iam_user.ci.name
-  # encrypt it using the @adanalife keybase key
-  pgp_key = "keybase:adanalife"
+
+## ci-terraform
+# create a special terraform role with extra permissions
+resource "aws_iam_role" "ci_terraform" {
+  name               = "CITerraformRole"
+  #TODO: custom policy here?
+  assume_role_policy = data.aws_iam_policy_document.ci_service_account_policy.json
+}
+
+resource "aws_iam_policy" "ci_terraform" {
+  name   = "AllowAccessForTerraform"
+  policy = <<EOF
+{
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+            "Sid": "S3ReadWriteAccess",
+            "Action": [
+              "s3:*Object",
+              "s3:ListBucket"
+            ],
+            "Effect": "Allow",
+            "Resource": [
+                "*"
+            ]
+        }
+    ]
+}
+EOF
+}
+
+# give the CI terraform role access to the terraform policy
+resource "aws_iam_role_policy_attachment" "ci_terraform" {
+  role       = aws_iam_role.ci_terraform.name
+  policy_arn = aws_iam_policy.ci_terraform.arn
 }
