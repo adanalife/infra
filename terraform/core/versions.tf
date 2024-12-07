d2f7fd2c4455550b6f3a375fc8d23bf26911977b
Dana Merrick <dmerrick@ginkgobioworks.com>
2024-12-07 16:19:53 -0600

    add debug

--- terraform/core/versions.tf
+++ terraform/core/versions.tf
@@ -11,5 +11,8 @@ terraform {
   backend "s3" {
     bucket = "adanalife-core-tf-state"
     key    = "adanalife-core.tfstate"
+    # assume_role = {
+    #   role_arn = "arn:aws:iam::${local.core_account_id}:role/Terraform"
+    # }
   }
 }
